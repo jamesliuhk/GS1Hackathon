16061681032beb0b8ac307abb32b2b7f3d5f702e
Meng Lyu <mlyu5@dons.usfca.edu>
2019-10-20 09:18:52 -0700

    Databases

--- app/src/main/java/com/example/yzxing/DatabaseHelper.java
+++ app/src/main/java/com/example/yzxing/DatabaseHelper.java
@@ -7,6 +7,9 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import static android.content.ContentValues.TAG;
 
 public class DatabaseHelper extends SQLiteOpenHelper {
@@ -18,23 +21,35 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     @Override
     public void onCreate(SQLiteDatabase db) {
         Log.v(TAG, "create Database------------->");
-        String createSQL1 = "CREATE TABLE companies (\n" +
+        String createSQL = "CREATE TABLE companies (\n" +
                 "  barcode VARCHAR(100) NOT NULL,\n" +
                 "  company VARCHAR(100) NOT NULL,\n" +
                 "  PRIMARY KEY (barcode))";
 
-        String createSQL2 = "CREATE TABLE oreo (\n" +
-                "  barcode VARCHAR(100) NOT NULL,\n" +
-                "  data VARCHAR(200),\n" +
-                "  supplier VARCHAR(200),\n" +
-                "  PRIMARY KEY (barcode))";
+//        String createSQL2 = "CREATE TABLE oreo (\n" +
+//                "  barcode VARCHAR(100) NOT NULL,\n" +
+//                "  data VARCHAR(200),\n" +
+//                "  supplier VARCHAR(200),\n" +
+//                "  PRIMARY KEY (barcode))";
 
-        db.execSQL(createSQL1);
-        db.execSQL(createSQL2);
+        db.execSQL(createSQL);
+        Map<String, String> comMap = new HashMap<>();                         //<barcode, company>"
+        comMap.put("0028028", "Lettuce");
+        comMap.put("0100169", "FreshCut");
+        comMap.put("0169422", "Coke");
+
         ContentValues values = new ContentValues();
-        values.put("barcode", "0467589");
-        values.put("company", "oreo");
-        db.insert("companies", null, values);
+        for (Map.Entry<String, String> entry : comMap.entrySet()) {
+            db.execSQL("CREATE TABLE " + entry.getValue() +" (\n" +
+                    "  barcode VARCHAR(100) NOT NULL,\n" +
+                    "  data VARCHAR(200),\n" +
+                    "  supplier VARCHAR(200),\n" +
+                    "  PRIMARY KEY (barcode))");
+            values.put("barcode", entry.getKey());
+            values.put("company", entry.getValue());
+            db.insert("companies", null, values);
+            values.clear();
+        }
     }
 
     @Override
